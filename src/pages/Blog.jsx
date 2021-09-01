77cf67ec809cd8bd6429b4732b7e395e433a5110
Artem <a.4ggame@gmail.com>
2021-09-01 14:59:44 +0300

    fast

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import {useDispatch, useSelector} from "react-redux";
+import {getUserSW} from "../redux/ducks/userSlice";
 
 const Blog = () => {
+    const dispatch = useDispatch();
+
+    React.useEffect(()=>{
+        dispatch({type:getUserSW.type})
+    },[])
+const planets = useSelector(state=>state.user.planets.results)
+
     return (
         <div>
-Blog
+            {planets?.map((item,index)=>{
+                return(
+                    <div key={index}>
+                        {item.name}
+                    </div>
+                )
+            })}
         </div>
     );
 };
